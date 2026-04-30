cf6ea1ac3fea892bee1b209b0d7470d3a4ac48a9
ly <luo1732913827@qq.com>
2026-04-30 12:09:33 +0800

    add homework_07 folder

---
+++ 03_Firmware/homework_07/MDK-ARM/RTE/_homework_06/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'homework_06' 
+ * Target:  'homework_06' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 03_Firmware/homework_07/MDK-ARM/homework_06/heap_4.d
@@ -0,0 +1,13 @@
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+homework_06\heap_4.o: D:\Program Files (x86)\keil\ARM\ARMCC\Bin\..\include\stdlib.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+homework_06\heap_4.o: D:\Program Files (x86)\keil\ARM\ARMCC\Bin\..\include\stddef.h
+homework_06\heap_4.o: D:\Program Files (x86)\keil\ARM\ARMCC\Bin\..\include\stdint.h
+homework_06\heap_4.o: ../Core/Inc/FreeRTOSConfig.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+homework_06\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
